847901e5a94684adccb142d65b982c599364f1da
Marian Šámal <margotka000@gmail.com>
2023-06-15 13:34:45 +0200

    i hate bootstrap

--- src/components/current-match.tsx
+++ src/components/current-match.tsx
@@ -3,6 +3,7 @@ import { useGameLogic } from '../logic';
 import Card from 'react-bootstrap/Card';
 import Table from 'react-bootstrap/Table';
 import { GoalRow } from './goal-row';
+import { Col, Row } from 'react-bootstrap';
 
 const vsInlineStyle: React.CSSProperties = {
     marginTop: 'auto',
@@ -30,17 +31,22 @@ export const CurrentMatch: React.FC = () => {
         <Card className='text-center mb-4'>
             <Card.Header>Probíhající zápas ({game.no + 1}. z 45)</Card.Header>
             <Card.Body className='d-flex flex-row justify-content-around' style={{marginBottom: -10}}>
-                <div className='d-flex flex-column flex-grow-1'>
-                    <Card.Text style={{fontSize: 30}}>{team1.name}</Card.Text>
-                    <Card.Text style={{fontSize: 80, fontWeight: 600, marginTop: -25}} >{game.goals1}</Card.Text>
-                </div>
+                <Row className='w-100'>
+                    <Col xs={5}>
+                        <Card.Text style={{fontSize: 30}}>{team1.name}</Card.Text>
+                        <Card.Text style={{fontSize: 80, fontWeight: 600, marginTop: -25}} >{game.goals1}</Card.Text>
+                    </Col>
 
-                <Card.Text style={vsInlineStyle}>vs</Card.Text>
+                    <Col xs={2} className='my-auto'>
+                        <Card.Text style={vsInlineStyle}>vs</Card.Text>
+                    </Col>
 
-                <div className='d-flex flex-column flex-grow-1'>
-                    <Card.Text style={{fontSize: 30}}>{team2.name}</Card.Text>
-                    <Card.Text style={{fontSize: 80, fontWeight: 600, marginTop: -25}} >{game.goals2}</Card.Text>
-                </div>
+
+                    <Col xs={5}>
+                        <Card.Text style={{fontSize: 30}}>{team2.name}</Card.Text>
+                        <Card.Text style={{fontSize: 80, fontWeight: 600, marginTop: -25}} >{game.goals2}</Card.Text>
+                    </Col>
+                </Row>
             </Card.Body>
             {!!game.goals.length && (
                 <Table bordered style={{tableLayout: 'fixed', marginBottom: 0, marginRight: 0}}>
